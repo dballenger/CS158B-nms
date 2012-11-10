428a0bdbcf40a206c9b5ab94e632e6a62c6c6e92
Daniel Ballenger <dballenger@denetron.com>
2012-11-10 12:03:56 -0800

    Base rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nms::Application.config.secret_token = 'f9351d7f198b3b73b62af1c0c2ac19755cd97f2f1eb38e2d9ea46b2cebd1d421d96e5caa86db2b9b2c3aa06fe3e5194bad0342f0657ddca0459c71160c16755d'
