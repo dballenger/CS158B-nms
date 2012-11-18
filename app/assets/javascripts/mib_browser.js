194c7f0a6adc0c020006ef2c0c66087be10da4b3
Daniel Ballenger <dballenger@denetron.com>
2012-11-18 13:13:40 -0800

    The javascript now works fine.  Forgot you have to pass in an anonymous function.

--- app/assets/javascripts/mib_browser.js
+++ app/assets/javascripts/mib_browser.js
@@ -1,15 +1,13 @@
-$.ready(
+$(document).ready(function() {
   $("input[name='mib_filter']").keyup(function() {
     var filter = $(this).val();
-    var count = 0;
     
     $("div.mib-list ul li").each(function() {
       if ($(this).text().search(new RegExp(filter, "i")) < 0) {
         $(this).fadeOut();
       } else {
         $(this).show();
-        count++;
       }
     });
   })
-);
+});
